4bdab990cefec4a7c19508faef36858c2a5cc255
Lay Sukmin Lim <imsukmin@gmail.com>
2014-05-04 06:12:43 +0900

    impliment checkbox intent impliment start-pause button impliment setting to main intent

--- FiveMinLeft/src/com/fiveminleft/MainActivity.java
+++ FiveMinLeft/src/com/fiveminleft/MainActivity.java
@@ -1,49 +1,94 @@
 package com.fiveminleft;
 
-import android.app.Activity; 
-import android.os.Bundle; 
-import android.os.Handler; 
-import android.os.SystemClock; 
-import android.view.Menu;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
 	private TextView textTimer; 
-	private Button startButton; 
-	private Button pauseButton; 
+	private TextView[] setTime = new TextView[10];
+
+	private Button stopButton; 
+	private ToggleButton spButton; //Start-Pause Button
 	private long startTime = 0L; 
 	private Handler myHandler = new Handler(); 
 	long timeInMillies = 0L; 
 	long timeSwap = 0L; 
 	long finalTime = 0L;
-
+	boolean[] checkState = new boolean[10];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		textTimer = (TextView) findViewById(R.id.textTimer);
+		spButton = (ToggleButton) findViewById(R.id.btnStartStop);
+		
+		setTime[0] = (TextView)findViewById(R.id.timeView1);
+		setTime[1] = (TextView)findViewById(R.id.timeView2);
+		setTime[2] = (TextView)findViewById(R.id.timeView3);
+		setTime[3] = (TextView)findViewById(R.id.timeView4);
+		setTime[4] = (TextView)findViewById(R.id.timeView5);
+		setTime[5] = (TextView)findViewById(R.id.timeView6);
+		setTime[6] = (TextView)findViewById(R.id.timeView7);
+		setTime[7] = (TextView)findViewById(R.id.timeView8);
+		setTime[8] = (TextView)findViewById(R.id.timeView9);
+		setTime[9] = (TextView)findViewById(R.id.timeView10);
 
-		startButton = (Button) findViewById(R.id.btnStart);
-		startButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				startTime = SystemClock.uptimeMillis();
-				myHandler.postDelayed(updateTimerMethod, 0);
+		for (int i = 0; i < checkState.length; i++) {
+			checkState[i] = false;	
+		}
+		Intent intent = getIntent();
+		checkState = intent.getBooleanArrayExtra("checkStare");
+		for (int i = 0; i < 10; i++) {
+			if(!checkState[i]){
+				setTime[i].setTextColor(Color.BLACK);
+				setTime[i].setText("it is NOT check");
+			} else {
+				setTime[i].setTextColor(Color.BLUE);
+				setTime[i].setText("it is check");				
+			}
+			
+		}
+		
+		spButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+		            // The toggle is enabled
+					timeSwap = 0L;
+					startTime = SystemClock.uptimeMillis();
+					myHandler.postDelayed(updateTimerMethod, 0);
+		        } else {
+		            // The toggle is disabled
+					timeSwap += timeInMillies;
+					myHandler.removeCallbacks(updateTimerMethod);
+		        }
 			}
 		});
-		pauseButton = (Button) findViewById(R.id.btnPause);
-		pauseButton.setOnClickListener(new View.OnClickListener() {
+		
+		stopButton = (Button) findViewById(R.id.btnPause);
+		stopButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
-				timeSwap += timeInMillies;
-				myHandler.removeCallbacks(updateTimerMethod);
-
+				finish();
 			}
 		});
 
 	}
+
+	
 	private Runnable updateTimerMethod = new Runnable() {
 
 		public void run() {
--- FiveMinLeft/src/com/fiveminleft/SettingActivity.java
+++ FiveMinLeft/src/com/fiveminleft/SettingActivity.java
@@ -1,14 +1,48 @@
 package com.fiveminleft;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class SettingActivity extends Activity {
-
+	
+	final boolean[] isChecked = new boolean[10];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.setting_activitiy);
+		setContentView(R.layout.setting_activity);
+		
+		Button startButton = (Button)findViewById(R.id.btnStartAtSetting);
+		final CheckBox[] cb = new CheckBox[10];
+		cb[0] = (CheckBox)findViewById(R.id.CheckBox00);
+		cb[1] = (CheckBox)findViewById(R.id.CheckBox01);
+		cb[2] = (CheckBox)findViewById(R.id.CheckBox02);
+		cb[3] = (CheckBox)findViewById(R.id.CheckBox03);
+		cb[4] = (CheckBox)findViewById(R.id.CheckBox04);
+		cb[5] = (CheckBox)findViewById(R.id.CheckBox05);
+		cb[6] = (CheckBox)findViewById(R.id.CheckBox06);
+		cb[7] = (CheckBox)findViewById(R.id.CheckBox07);
+		cb[8] = (CheckBox)findViewById(R.id.CheckBox08);
+		cb[9] = (CheckBox)findViewById(R.id.CheckBox09);
+		
+		startButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub 
+				for (int i = 0; i < isChecked.length; i++) {
+					isChecked[i] = cb[i].isChecked();
+				}
+				Intent intent = new Intent(SettingActivity.this, MainActivity.class);
+				intent.putExtra("checkStare", isChecked);
+				startActivity(intent);				
+			}
+		});
 		
 	}
 }
